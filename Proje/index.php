87ece42399e76aff9badeae3f23275ea06b01ae5
ÖZLEM <126457800+ozlemgurlekk@users.noreply.github.com>
2023-07-25 14:59:08 +0300

    paylaştıkça
    tasarım denemesi

---
+++ Proje/index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <meta charset="UTF-8">
+
+    <title>Anasayfa | Paylaştıkça </title>
+    <link rel="stylesheet" href="css/style1.css">
+
+    <script src="https://kit.fontawesome.com/c20485228a.js" crossorigin="anonymous"></script>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https: //fonts.googleapis.com/css2?family= Nunito :wght@300 & display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+</head>
+
+<body>
+
+    <section id="menu">
+        <div id="logo">Paylaştıkça</div>
+        <nav>
+            <a href=""><i class="fas fa-home  ikon"></i>Anasayfa</a>
+            <a href=""><i class="fas fa-info  ikon"></i>Hakkımızda</a>
+            <a href=""><i class="fas fa-graduation-cap ikon"></i>Eğitimler</a>
+            <a href=""><i class="fas fa-user-friends ikon"></i>Ekip</a>
+            <a href=""><i class="fas fa-map-pin ikon"></i>İletişim</a>
+        </nav>
+
+    </section>
+    <section id="anasayfa">
+        <div id="black">
+
+        </div>
+        <div id="icerik">
+            <h2>Paylaştıkça</h2>
+            <hr width="300" align="left">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit
+                In veniam laborum ullam perferendis sint libero nobis.
+                Ullam facilis quos beatae, minima
+                non sapiente esse quia aperiam
+                similique omnis obcaecati iusto?</p>
+        </div>
+
+    </section>
+
+    <section class="hakkimizda">
+        <h3>Hakkımızda </h3>
+        <div id="container">
+            <div id="sol">
+                <h5 id="h5sol">
+                    Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit.
+                    Necessitatibus
+                </h5>
+
+            </div>
+
+            <div id="sag">
+                <span>L</span>
+                <p id="psag">orem ipsum dolor sit amet consectetur adipisicing elit.
+                    Incidunt id atque, debitis, nemo pariatur blanditiis
+                    earum magnam quibusdam voluptate quam minus!
+                    Quae delectus accusantium cum expedita ratione,
+                    iusto sunt natus!</p>
+
+            </div>
+            <img src="C:\Users\HP\Desktop\Proje\img-20230617T193508Z-001\img\about.jpg" alt="" class="img-fluid mt100">
+
+            <p id="pson">
+
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eius harum,
+                veritatis sunt magni laborum a porro recusandae non quam
+                blanditiis saepe sequi rerum at praesentium placeat
+                deserunt culpa autem modi?
+
+            </p>
+
+        </div>
+
+    </section>
+    <section id="egitimler">
+        <div class="container">
+
+            <h3>Eğitimler</h3>
+
+            <div class="owl-carousel owl-theme">
+
+                <div class="card item " data-merge="1.5">
+                    <img src="C:\Users\HP\Desktop\Proje\img-20230617T193508Z-001\img\r2.jpg" alt="" class="img-fluid">
+                    <h5 class="baslikcard">HTML5 ve CSS3 Eğitimi</h5>
+                    <p class="cardp">Lorem ipsum dolor,
+                        sit amet consectetur adipisicing elit.
+                        Ab, quo!</p>
+                </div>
+
+                <div class="card item" data-merge="1.5">
+                    <img src="C:\Users\HP\Desktop\Proje\img-20230617T193508Z-001\img\r2.jpg" alt="" class="img-fluid">
+                    <h5 class="baslikcard">HTML5 ve CSS3 Eğitimi</h5>
+                    <p class="cardp">Lorem ipsum dolor,
+                        sit amet consectetur adipisicing elit.
+                        Ab, quo!</p>
+                </div>
+
+                <div class="card item " data-merge="1.5">
+                    <img src="C:\Users\HP\Desktop\Proje\img-20230617T193508Z-001\img\r2.jpg" alt="" class="img-fluid">
+                    <h5 class="baslikcard">HTML5 ve CSS3 Eğitimi</h5>
+                    <p class="cardp">Lorem ipsum dolor,
+                        sit amet consectetur adipisicing elit.
+                        Ab, quo!</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <section id="ekip">
+        <div class="container">
+            <h3 id="ekiph3">Ekip</h3>
+
+            <div class="sutun-sol-sag">
+                <img src="C:\Users\HP\Desktop\Proje\img-20230617T193508Z-001\img\ekip1.jpg" alt="" class="img-fluid oval ">
+                <h4 class="ekipisim"> Mauro Icardi</h4>
+                <p class="ekipp">Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit.
+                    At, voluptate?</p>
+
+                <div class="ekip-ikon">
+
+                    <a href="#"><i class="fab fa-facebook-f social"></i></a>
+                    <a href="#"><i class="fab fa-instagram social"></i></a>
+                    <a href="#"><i class="fab fa-whatsapp social"></i></a>
+
+                </div>
+            </div>
+
+            <div class="sutun ">
+                <img src="C:\Users\HP\Desktop\Proje\img-20230617T193508Z-001\img\ekip2.jpg" alt="" class="img-fluid oval ">
+                <h4 class="ekipisim">Merve Gürlek</h4>
+                <p class="ekipp">Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit.
+                    At, voluptate?</p>
+
+                <div class="ekip-ikon">
+
+                    <a href="#"><i class="fab fa-facebook-f social"></i></a>
+                    <a href="#"><i class="fab fa-instagram social"></i></a>
+                    <a href="#"><i class="fab fa-whatsapp social"></i></a>
+
+                </div>
+            </div>
+
+            <div class="sutun-sol-sag">
+                <img src="C:\Users\HP\Desktop\Proje\img-20230617T193508Z-001\img\ekip3.jpg" alt="" class="img-fluid oval ">
+                <h4 class="ekipisim">Deniz Çelik</h4>
+                <p class="ekipp">Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit.
+                    At, voluptate?</p>
+
+                <div class="ekip-ikon">
+
+                    <a href="#"><i class="fab fa-facebook-f social"></i></a>
+                    <a href="#"><i class="fab fa-instagram social"></i></a>
+                    <a href="#"><i class="fab fa-whatsapp social"></i></a>
+
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <section id="iletisim">
+        <div class="container">
+            <h3 id="h3iletisim">İletişim</h3>
+
+            <form action="index.php" method="post">
+
+                <div id="iletisimopak">
+            </form>
+
+            <div id="formgroup">
+                <div id="solform">
+                    <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+                    <input type="text" name="tel" placeholder="Telefon Numarası" required class="form-control">
+                </div>
+                <div id="sagform">
+                    <input type="email" name="mail" placeholder="Email Adresiniz" required class="form-control">
+                    <input type="text" name="konu" placeholder="Konu Başlığı" required class="form-control">
+                </div>
+
+                <textarea name="mesaj" id="" cols="30" placeholder="Mesaj Giriniz" rows="10" required class="form-control"></textarea>
+
+                <input type="submit" value="Gönder">
+
+            </div>
+
+            <div id="adres">
+
+                <h4 id="adresbaslik">Adres :</h4>
+
+                <p class="adresp">Mehmet Akif Ersoy Mah. </p>
+                <p class="adresp">Akyıldız Caddesi</p>
+                <p class="adresp">Oguz Bey Sokak No:123</p>
+                <p class="adresp">0212 389 99 96</p>
+                <p class="adresp">Email : paylastikca@paylaastikca.com</p>
+
+            </div>
+
+        </div>
+
+        <footer>
+
+            <div id="copyright">2020 | Tüm hakları saklıdır</div>
+
+            <div id="socialfooter">
+
+                <a href="#"><i class="fab fa-facebook-f social"></i></a>
+                <a href="#"><i class="fab fa-instagram social"></i></a>
+                <a href="#"><i class="fab fa-whatsapp social"></i></a>
+
+            </div>
+            <a href="#menu"><i class="fas fa-angle-up" id="up"></i></a>
+
+        </footer>
+
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>
+
+    <script src="owl/owl.carousel.min.js"></script>
+    <script src="owl/script.js"></script>
+</body>
+
+</html>
+
+<?php
+
+include("baglanti.php");
+
+if (isset($_POST["isim"], $_POST["tel"], $_POST["mail"], $_POST["konu"], $_POST["mesaj"])) {
+    $adsoyad = $_POST["isim"];
+    $telefon = $_POST["tel"];
+    $email = $_POST["mail"];
+    $konu = $_POST["konu"];
+    $mesaj = $_POST["mesaj"];
+
+
+    $ekle = "INSERT INTO iletisim (adsoyad, telefon, email, konu, mesaj)
+     VALUES ('" . $adsoyad . "','555','mail@mail.com','TEST','mesajım')";
+}
+
+
+?>
